e745830a29fa27ddd3fa8627861ee7b491d0511d
Patrick Olvera <patrickolvera@gmail.com>
2019-02-28 15:04:20 -0600

    feat: Loading Bar functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,30 @@
 /***********************************************
  * TODO:
- * -Search:
- *   -loading animation during request
  * -Results:
  *   -create book components to be rendered
  *   -put books on the page
  *   -Link to more information
- *    -perform volume request (https://developers.google.com/books/docs/v1/using#RetrievingVolume)
+ *    -perform volume request or redirect (https://developers.google.com/books/docs/v1/using#RetrievingVolume)
  * -Bonus:
  *   -handle errors with API requests
  *   -display error message to user when submiting non valid query
  *   -write tests for components
  *   -use Redux
  *   -write tests for Redux
+ *   -add animations to books
  ************************************************/
 
 import React, { Component } from "react";
 import SearchBox from "../SearchBox";
+import LoadingBar from "../LoadingBar";
 import getBooks from "../../API";
 
 class App extends Component {
   state = {
     query: "",
-    books: []
+    books: [],
+    loading: false,
+    loadingBar: false
   };
   handleSearchInput = event => {
     this.setState({
@@ -31,15 +33,21 @@ class App extends Component {
   };
   handleSearchSubmit = async event => {
     event.preventDefault();
+    this.setState({ loading: true, loadingBar: true });
     const books = await getBooks(this.state.query);
-    this.setState({ books });
-    console.log(this.state.books);
+    this.setState({ books, loading: false });
+    setTimeout(() => {
+      this.setState({ loadingBar: false });
+    }, 500);
   };
   render() {
+    const { query, loading, loadingBar } = this.state;
+
     return (
       <div>
+        {loadingBar ? <LoadingBar loading={loading} /> : null}
         <SearchBox
-          query={this.state.query}
+          query={query}
           handleSearchInput={this.handleSearchInput}
           handleSearchSubmit={this.handleSearchSubmit}
         />
---
+++ src/components/LoadingBar/LoadingBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles, LinearProgress } from "@material-ui/core";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  hide: {
+    display: "none"
+  }
+};
+class LoadingBar extends Component {
+  state = {
+    completed: 0
+  };
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  progress = () => {
+    const { completed } = this.state;
+    if (this.props.loading) {
+      this.setState({ completed: completed + 30 });
+    } else {
+      this.setState({ completed: 100 });
+    }
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <LinearProgress variant="determinate" value={this.state.completed} />
+      </div>
+    );
+  }
+}
+
+LoadingBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(LoadingBar);
